1f71440fccca86811c294b32bc2bb61352088215
Zubair Nagori <mac@macs-macbook-pro.local>
2018-10-15 20:38:17 +0530

    Convert to MVC design pattern.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-     var itemArray = ["Milk" , "Vegetable" ,"Oil"]
+     var itemArray = [Item]()
     
      let defaults = UserDefaults.standard
     
@@ -18,11 +18,27 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let  items = defaults.array(forKey: "TodoListArray") as? [String] {
+        let newItem = Item()
+        newItem.title = "Milk"
+        
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "a"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "b"
+        itemArray.append(newItem3)
+        
+        
+        
+        
+        
+        if let  items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
     }
-    
     //Mark - TableView Datasource Methods
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,8 +48,22 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) as! UITableViewCell
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+//        print("CellForRowAtIndexPathCalled")
         
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        // Ternary Operator
+        //  value = condition ? valueIfTrue : valueIfFalse
+        
+        
+        cell.accessoryType = item.done == true ? .checkmark : .none
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        }else{
+//            cell.accessoryType = .none
+//        }
         
         return cell
         
@@ -45,6 +75,16 @@ class TodoListViewController: UITableViewController {
         
         //print(itemArray[indexPath.row])
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        }
+//        else{
+//            itemArray[indexPath.row].done = false
+//        }
+        
+        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         }
@@ -52,7 +92,9 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
     // Mark - Add new items
@@ -65,9 +107,14 @@ class TodoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the Add item button on our UIAlert
+           
+            let newItem = Item()
+            newItem.title = textField.text!
+            
             if textField.text != nil {
-            self.itemArray.append(textField.text!)
+            self.itemArray.append(newItem)
             }
+            
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
@@ -85,4 +132,5 @@ class TodoListViewController: UITableViewController {
     
 
 }
+
 
---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by mac on 14/10/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+}
